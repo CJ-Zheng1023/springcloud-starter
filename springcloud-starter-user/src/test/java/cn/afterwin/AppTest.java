7eae20a05a571ba785f5bf259b81f401ff4d6692
AfterWin <CJ_Zheng1023@hotmail.com>
2019-07-26 15:27:40 +0800

    new commit

--- springcloud-starter-user/src/test/java/cn/afterwin/AppTest.java
+++ springcloud-starter-user/src/test/java/cn/afterwin/AppTest.java
@@ -1,20 +1,25 @@
 package cn.afterwin;
 
-import static org.junit.Assert.assertTrue;
 
+import cn.afterwin.domain.User;
+import cn.afterwin.service.UserService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+    @Autowired
+    private UserService userService;
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void showUserTest(){
+        User user = userService.showUser(1);
     }
+
 }
